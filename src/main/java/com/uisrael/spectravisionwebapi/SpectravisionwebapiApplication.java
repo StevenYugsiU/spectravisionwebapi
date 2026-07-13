cf6da6cc83223d6c87cc744e428282966e02258c
Cristhian Ganchala <cristhian89israel@gmail.com>
2026-07-12 20:11:41 -0500

    Inicialización de front

---
+++ src/main/java/com/uisrael/spectravisionwebapi/SpectravisionwebapiApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.spectravisionwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectravisionwebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectravisionwebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/spectravisionwebapi/SpectravisionwebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.spectravisionwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectravisionwebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

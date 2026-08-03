e39c6e5882f2670f4b0f82e890b0e20b7fec7ad4
Cristhian Ganchala <cristhian89israel@gmail.com>
2026-08-03 18:12:59 -0500

    Cambios locales en formularios y en entregas

---
+++ run-dev.cmd
@@ -0,0 +1,3 @@
+@echo off
+set "JAVA_HOME=C:\Program Files\Android\openjdk\jdk-21.0.8"
+call "%~dp0mvnw.cmd" spring-boot:run
--- src/main/resources/static/assets/js/select2.js
+++ src/main/resources/static/assets/js/select2.js
@@ -2,9 +2,14 @@
   'use strict';
 
   if ($(".js-example-basic-single").length) {
-    $(".js-example-basic-single").select2();
+    $(".js-example-basic-single").select2({
+      theme: 'bootstrap',
+      language: {
+        noResults: function() { return "Sin resultados"; }
+      }
+    });
   }
   if ($(".js-example-basic-multiple").length) {
-    $(".js-example-basic-multiple").select2();
+    $(".js-example-basic-multiple").select2({ theme: 'bootstrap' });
   }
 })(jQuery);
